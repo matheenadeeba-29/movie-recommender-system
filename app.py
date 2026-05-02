7b981c31830cf890e3929c7bd0643f025dd607fe
matheenadeeba-29 <matheenadeeba@gmail.com>
2026-05-02 23:59:47 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,6 +3,19 @@ import pickle
 import os
 import gdown
 
+import requests
+
+def fetch_poster(movie_id):
+    api_key = "22130acc2554f50d77f5399e4e172c37"
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}"
+    data = requests.get(url).json()
+    poster_path = data.get('poster_path')
+
+    if poster_path:
+        return "https://image.tmdb.org/t/p/w500/" + poster_path
+    else:
+        return "https://via.placeholder.com/500x750?text=No+Image"
+
 # ---------------- DOWNLOAD FILES FIRST ----------------
 MOVIES_URL = "https://drive.google.com/uc?id=1tgHUuo7lnXoogvpfoFzcp7NC1iQUYiNC"
 SIMILARITY_URL = "https://drive.google.com/uc?id=1TLUcp32DsVl2eXS6nv2-S2UL1UoJ_J8p"
@@ -29,21 +42,17 @@ movies, similarity = load_data()
 def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
-
-    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:10]
+    movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
 
     recommended_movies = []
-    seen = set()
+    recommended_posters = []
 
-    for i in movies_list:
-        title = movies.iloc[i[0]].title
-        if title not in seen:
-            recommended_movies.append(title)
-            seen.add(title)
-        if len(recommended_movies) == 5:
-            break
+    for i in movie_list:
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movies.append(movies.iloc[i[0]].title)
+        recommended_posters.append(fetch_poster(movie_id))
 
-    return recommended_movies
+    return recommended_movies, recommended_posters
 # ---------------- UI ----------------
 st.title("🎬 Movie Recommender System")
 
@@ -53,10 +62,11 @@ selected_movie = st.selectbox(
 )
 
 if st.button("Recommend"):
-    recommended_movies = recommend(selected_movie)
+    names, posters = recommend(selected_movie)
 
     col1, col2, col3, col4, col5 = st.columns(5)
 
     for i, col in enumerate([col1, col2, col3, col4, col5]):
         with col:
-            st.text(recommended_movies[i])
+            st.image(posters[i])
+            st.caption(names[i])
